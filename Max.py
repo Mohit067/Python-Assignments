539d59b56f9881f22469b9ccae65c6d8d38ff87f
Mohit067 <134305384+Mohit067@users.noreply.github.com>
2023-06-10 18:51:05 +0530

    Create Max.py

---
+++ Max.py
@@ -0,0 +1,17 @@
+# Python program to find greatest 
+
+# number in a list
+
+ 
+
+# list of numbers
+
+list1 = [10, 20, 1, 45, 99]
+
+ 
+
+ 
+
+# printing the maximum element
+
+print("Largest element is:", max(list1))
